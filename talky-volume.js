af13d7e9f1bad2f49ef20c268f29ad5202eb4f5f
apajaron <apajaron@tuenti.com>
2019-03-28 14:24:10 -0400

    Update slide bar when toggling mute.

--- talky-volume.js
+++ talky-volume.js
@@ -7,8 +7,6 @@ const addVolumeSlider = (e) => {
     slider.value = 100;
     slider.step = 1;
 
-    let oldVolume = slider.value;
-
     const toggleMute = (element, icon) => {
         [].slice.call(element.previousSibling.childNodes)
             .flatMap(e => [].slice.call(e.childNodes))
@@ -29,12 +27,11 @@ const addVolumeSlider = (e) => {
     slider.addEventListener('change', event => {
         const newVolume = event.target.value;
         adjustVolume(event.target, newVolume);
-        if (newVolume == 0) {
+        if (newVolume === "0") {
             toggleMute(event.target, 'VolumeIcon');
-        } else if (oldVolume == 0) {
+        } else {
             toggleMute(event.target, 'VolumeOffIcon');
         }
-        oldVolume = newVolume;
     });
     e.appendChild(slider);
 };
@@ -53,3 +50,22 @@ const addVolumeSliders = () => {
 
 // Check for users to join
 const checker = setInterval(addVolumeSliders, 1000);
+
+document.querySelector('body').addEventListener('click', function(e) {
+    let target = e.target;
+    while (target != null){
+        if (target.classList.contains('TalkyButton')){
+            let slider = target.parentNode.nextSibling;
+            if (slider && slider.classList.contains('volume')) {
+                if(slider.value === "0") {
+                    slider.value = slider.dataset.volumeBeforeMute;
+                } else if (!target.classList.contains('TalkyButton__off')) {
+                    slider.dataset.volumeBeforeMute = slider.value;
+                    slider.value = 0;
+                }
+            }
+            return;
+        }
+        target = target.parentElement;
+    }
+}, true);
